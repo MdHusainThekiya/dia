cffcd722f2599cfa96e0005220e8e542d4052b7b
Md Husain Thekiya <hussainthekiya@gmail.com>
2025-09-13 16:53:00 +0530

    fix version change

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ if [ "$1" == "--install" ]; then
   apt-get update -y
   apt-get install -y python3.10 python3.10-venv python3.10-distutils python3.10-dev
   # Point python3 to Python 3.10
-  !sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
-  !sudo update-alternatives --config python3
+  sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+  sudo update-alternatives --config python3
 
   pip install uv==0.6.14
 
